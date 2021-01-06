74cb9ef39e7c57b56668854e750d0ac0cb3368f0
Philippe Chataignon <philippe.chataignon@dgfip.finances.gouv.fr>
2021-01-06 08:41:47 +0000

    'by' is optionnal in row_number functions

--- R/row_number_by.R
+++ R/row_number_by.R
@@ -8,8 +8,15 @@
 #' dt2 <- data.table(id1 = rep(1:100, each=50), id2 = rep(1:1000, each=5))
 #' dt2[, row_num := row_number_by(dt, c("id1", "id2"))]
 #' @export
-row_number_by <- function(dt, by) {
-  Crow_number_by(dt[,.N], group(dt, by))
+row_number_by <- function(dt, by=NULL) {
+  if (is.null(by)) {
+    grp = numeric(0)
+    attr(grp, "starts") = 1
+    attr(grp, "maxgrpn") = 1
+  } else {
+    grp = group(dt, by)
+  }
+  Crow_number_by(dt[,.N], grp)
 }
 
 #' @title Returns a logical TRUE for first row of each group
@@ -21,8 +28,15 @@ row_number_by <- function(dt, by) {
 #' dt[first_by(dt, "id"), pos := "F"]
 #' dt[last_by(dt, "id"), pos := "L"]
 #' @export
-first_by <- function(dt, by) {
-  Cfirst_by(dt[,.N], group(dt, by))
+first_by <- function(dt, by=NULL) {
+  if (is.null(by)) {
+    grp = numeric(0)
+    attr(grp, "starts") = 1
+    attr(grp, "maxgrpn") = 1
+  } else {
+    grp = group(dt, by)
+  }
+  Cfirst_by(dt[,.N], grp)
 }
 
 #' @title Returns a logical TRUE for last row of each group
@@ -34,6 +48,13 @@ first_by <- function(dt, by) {
 #' dt[first_by(dt, "id"), pos := "F"]
 #' dt[last_by(dt, "id"), pos := "L"]
 #' @export
-last_by <- function(dt, by) {
-  Clast_by(dt[,.N], group(dt, by))
+last_by <- function(dt, by=NULL) {
+  if (is.null(by)) {
+    grp = numeric(0)
+    attr(grp, "starts") = 1
+    attr(grp, "maxgrpn") = 1
+  } else {
+    grp = group(dt, by)
+  }
+  Clast_by(dt[,.N], grp)
 }
