afc7c506614f334619e1b8586440a528cea28d5b
Philippe Chataignon <philippe.chataignon@dgfip.finances.gouv.fr>
2021-05-21 18:53:56 +0000

    Add DT as [ wrapper

--- R/internals.R
+++ R/internals.R
@@ -1,3 +1,11 @@
 group <- function(dt, by) {
   data.table:::forderv(dt, by = by, retGrp = T)
 }
+
+#' @title wrapper of [
+#' @examples
+#' dt <- data.table(a = c(1:5, na, 7:10), b=rnorm(10), c=letters[1:10])
+#'
+#' DT(dt, a %% 2 == 0, .(b, d=2*b))
+#' @export
+DT <- data.table:::"[.data.table"
