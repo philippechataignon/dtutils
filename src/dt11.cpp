b36648715024de01f67790e2a67ecdc9363a9934
Philippe Chataignon <philippe.chataignon@gmail.com>
2023-01-22 16:19:43 +0100

    Simplest version

--- src/dt11.cpp
+++ src/dt11.cpp
@@ -5,13 +5,12 @@ namespace writable = cpp11::writable;
 [[cpp11::register]]
 logicals Cfirst_by(int n, integers rows, integers grps) {
   writable::logicals ret(n);
+  for(int i=0; i < ret.size(); i++) {
+    ret[i] = FALSE;
+  }
   for(int g=0; g < grps.size(); g++) {
     int f = grps[g] - 1; // start indice of group g (C indice = R indice - 1)
-    int l = g == (grps.size() - 1) ? n : grps[g + 1] - 1; // last indice (n if last group)
-    for(int i = f; i < l; i++) {
-      int r  = rows.size() == 0 ? i : rows[i] - 1;
-      ret[r] = i == f;
-    }
+    ret[f] = TRUE;
   }
   return ret;
 }
