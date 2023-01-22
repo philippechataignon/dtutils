5c50876a95119d491097791e8ae769719568f39f
Philippe Chataignon <philippe.chataignon@gmail.com>
2023-01-22 15:50:10 +0100

    Use size()

--- src/dt11.cpp
+++ src/dt11.cpp
@@ -4,15 +4,12 @@ namespace writable = cpp11::writable;
 
 [[cpp11::register]]
 logicals Cfirst_by(int n, integers rows, integers grps) {
-  int nrows = rows.size();
-  R_xlen_t ngrps = grps.size();
   writable::logicals ret(n);
-
-  for(int g=0; g<ngrps; g++) {
+  for(int g=0; g < grps.size(); g++) {
     int f = grps[g] - 1; // start indice of group g (C indice = R indice - 1)
-    int l = g == (ngrps - 1) ? n : grps[g + 1] - 1; // last indice (n if last group)
+    int l = g == (grps.size() - 1) ? n : grps[g + 1] - 1; // last indice (n if last group)
     for(int i = f; i < l; i++) {
-      int r  = nrows == 0 ? i : rows[i] - 1;
+      int r  = rows.size() == 0 ? i : rows[i] - 1;
       ret[r] = i == f;
     }
   }
