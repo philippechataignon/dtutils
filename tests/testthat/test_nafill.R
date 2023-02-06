8220c1b464907000597369d8d7f3f1e9e90db084
Philippe Chataignon <philippe.chataignon@gmail.com>
2023-02-06 12:24:23 +0100

    Review tests 2

--- tests/testthat/test_nafill.R
+++ tests/testthat/test_nafill.R
@@ -1,11 +1,12 @@
 context("na_fill_by")
 library(data.table)
 
-dt <- data.table(val=c(NA, 1, NA, NA, 2, NA, 3, NA))
+dt <- CJ(id=1:2, val=c(NA, 1, NA, NA, 2, NA, 3, NA), sorted=F)
+
 test_that("na_fill_elem", {
-  expect_equal(dt[, na_fill_by(dt=.SD, var="val", type=1)]$val, c(NA, 1, 1, 1, 2, 2, 3, 3))
-  expect_equal(dt[, na_fill_by(dt=.SD, var="val", type=2)]$val, c(1, 1, 2, 2, 2, 3, 3, NA))
-  expect_equal(dt[, na_fill_by(dt=.SD, var="val", type=3)]$val, c(1, 1, 1, 1, 2, 2, 3, 3))
+  expect_equal(dt[, na_fill_by(dt=.SD, var="val", by="id", type=1)]$val, c(NA, 1, 1, 1, 2, 2, 3, 3, NA, 1, 1, 1, 2, 2, 3, 3))
+  expect_equal(dt[, na_fill_by(dt=.SD, var="val", by="id", type=2)]$val, c(1, 1, 2, 2, 2, 3, 3, NA, 1, 1, 2, 2, 2, 3, 3, NA))
+  expect_equal(dt[, na_fill_by(dt=.SD, var="val", by="id", type=3)]$val, c(1, 1, 1, 1, 2, 2, 3, 3, 1, 1, 1, 1, 2, 2, 3, 3))
 })
 
 # pour compatibilité seed
@@ -30,12 +31,6 @@ dt[(1:.N %% 5 == 0), e:=NA]
 
 vars = c("a", "b", "c", "d", "e")
 
-dt0 <- copy(dt)
-system.time(na_fill_by(dt0, by="id", inplace=T))
-test_that("na_fill_by0", {
-  expect_equal(dt0[2, a], 5)
-})
-
 dt1 <- copy(dt)
 system.time(na_fill_by(dt1, var=vars, by="id", inplace=T))
 test_that("na_fill_by1", {
@@ -43,21 +38,37 @@ test_that("na_fill_by1", {
 })
 
 dt2 <- copy(dt)
-system.time(dt2 <- na_fill_by(dt2, var=vars, by="id"))
+system.time(dt2[, (vars) := na_fill_by(.SD, var=vars, by="id")])
 test_that("na_fill_by2", {
-  expect_equal(dt2[[1]][2], 5)
+  expect_equal(dt2[2, a], 5)
+  expect_identical(dt1, dt2)
 })
 
 dt3 <- copy(dt)
-system.time(dt3[, (vars) := na_fill_by(.SD, var=vars, by="id")])
+system.time(dt3[, na_fill_by(.SD, vars, "id", inplace=T)])
 test_that("na_fill_by3", {
   expect_equal(dt3[2, a], 5)
   expect_identical(dt1, dt3)
 })
 
+vars = c("a", "c", "d", "e")
+
 dt4 <- copy(dt)
-system.time(dt4[, na_fill_by(.SD, vars, "id", inplace=T)])
+system.time(na_fill_by(dt4, var=vars, by="id", inplace=T))
 test_that("na_fill_by4", {
   expect_equal(dt4[2, a], 5)
-  expect_identical(dt1, dt4)
+})
+
+dt5 <- copy(dt)
+system.time(dt5[, (vars) := na_fill_by(.SD, var=vars, by="id")])
+test_that("na_fill_by5", {
+  expect_equal(dt5[2, a], 5)
+  expect_identical(dt4, dt5)
+})
+
+dt6 <- copy(dt)
+system.time(dt6[, na_fill_by(.SD, vars, "id", inplace=T)])
+test_that("na_fill_by6", {
+  expect_equal(dt6[2, a], 5)
+  expect_identical(dt4, dt6)
 })
