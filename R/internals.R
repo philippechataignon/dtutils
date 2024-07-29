35accf41b0efc29443533f48f56234a07ccee386
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-07-29 20:25:58 +0200

    Remove DT useless

--- R/internals.R
+++ R/internals.R
@@ -1,11 +1,3 @@
 group <- function(dt, by) {
   data.table:::forderv(dt, by = by, retGrp = TRUE, sort = F)
 }
-
-#' @title wrapper of [
-#' @examples
-#' dt <- data.table(a = c(1:5, na, 7:10), b=rnorm(10), c=letters[1:10])
-#'
-#' DT(dt, a %% 2 == 0, .(b, d=2*b))
-#' @export
-DT <- data.table:::"[.data.table"
