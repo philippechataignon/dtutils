a7ff381978cd6a2e6810b6b488c8b4b1960a6d59
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-07-29 20:25:58 +0200

    Add is_lead, nop for n=0

--- src/shift_by.cpp
+++ src/shift_by.cpp
@@ -15,6 +15,7 @@ T Cshift_t(T x, IntegerVector rows, int shift, bool inplace = false) {
 
   IntegerVector grps = rows.attr("starts");
   R_xlen_t ngrps = grps.size();
+  bool is_lead = (shift > 0);
   bool is_lag = (shift < 0);
   if (is_lag) {
       shift = -shift;
@@ -26,7 +27,7 @@ T Cshift_t(T x, IntegerVector rows, int shift, bool inplace = false) {
   for(int g = 0; g < ngrps; ++g) {
     R_xlen_t f = grps[g] - 1; // start indice of group g (C indice = R indice - 1)
     R_xlen_t l = g == (ngrps - 1) ? n : grps[g + 1] - 1; // last indice (n if last group)
-    if (!is_lag) {
+    if (is_lead) {
       for(R_xlen_t i = f; i < l; ++i) {
         R_xlen_t r = nrows == 0 ? i : rows[i] - 1;
         if (i < l - shift) {
